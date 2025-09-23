6c18423101a3c2a540477eccaa1353d93753e987
patdalcia <patdalcia@gmail.com>
2025-09-23 16:46:28 -0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use macroquad::prelude::*;
+use macroquad::window;
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "My first game :)".to_string(),
+        fullscreen: true,   // start in fullscreen
+        window_width: 800,  // ignored when fullscreen = true
+        window_height: 600, // ignored when fullscreen = true
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    loop {
+        clear_background(BLACK);
+        next_frame().await
+    }
+}
+
